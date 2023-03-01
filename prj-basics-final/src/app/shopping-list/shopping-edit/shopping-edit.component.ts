af6b9ec51434f0cdcdded367d779673b852dfabb
Dimitar Iliev <ilievd@aforbes.com>
2023-03-01 20:34:57 +0200

    Add ingredient adder workflow

--- prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() ingredientEmitter = new EventEmitter<Ingredient>();
 
+  onAddClick(nameInput: string, amountInput: number): void {
+    var ingr = new Ingredient(nameInput, amountInput);
+    
+    this.ingredientEmitter.emit(ingr);
+  }
   constructor() { }
 
   ngOnInit() {
