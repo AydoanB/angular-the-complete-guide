604f751b47a47ab7927c1a2051d66f4ad06b81de
Aydoan Balakchiev <balakchiev02@gmail.com>
2023-03-11 18:45:32 +0200

    Remove commented code

--- prj-directives-final/src/app/recipes/recipe-outlet/recipe-outlet.component.ts
+++ prj-directives-final/src/app/recipes/recipe-outlet/recipe-outlet.component.ts
@@ -1,11 +1,16 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-outlet',
   templateUrl: './recipe-outlet.component.html',
   styleUrls: ['./recipe-outlet.component.css']
 })
-export class RecipeOutletComponent implements OnInit {
+export class RecipeOutletComponent implements OnInit{
+  selectedRecipe: Recipe;
+
+  constructor() {
+  }
   ngOnInit(): void {
   }
 }
