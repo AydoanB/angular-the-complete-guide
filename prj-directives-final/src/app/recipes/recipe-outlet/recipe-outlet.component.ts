0e557c766e0fe3f9080df4fdb43cabacd33d54f1
Aydoan Balakchiev <balakchiev02@gmail.com>
2023-03-11 09:59:41 +0200

    New recipe component

---
+++ prj-directives-final/src/app/recipes/recipe-outlet/recipe-outlet.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-outlet',
+  templateUrl: './recipe-outlet.component.html',
+  styleUrls: ['./recipe-outlet.component.css']
+})
+export class RecipeOutletComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
