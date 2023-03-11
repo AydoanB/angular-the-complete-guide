fbe6038085a386cd111d6d3ca9ca747dc0d9ae35
Aydoan Balakchiev <balakchiev02@gmail.com>
2023-03-11 18:41:37 +0200

    Add recipe edit component routes

--- prj-directives-final/src/app/app-routing.module.ts
+++ prj-directives-final/src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import { RouterModule, Routes} from "@angular/router";
 import {RecipesComponent} from "./recipes/recipes.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {RecipeListComponent} from "./recipes/recipe-list/recipe-list.component";
 import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
 import {RecipeOutletComponent} from "./recipes/recipe-outlet/recipe-outlet.component";
+import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 
 const  appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
       {path: '', component: RecipeOutletComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id/edit', component: RecipeEditComponent},
       {path: ':id', component: RecipeDetailComponent},
-      {path: 'list', component: RecipeListComponent}
     ]},
   {path: 'shopping-list', component: ShoppingListComponent, children: [
       {path: ':id/edit', component: ShoppingEditComponent}
