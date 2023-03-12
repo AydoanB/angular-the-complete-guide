cfb1b07ff3f8a172c7839b56766cb4d2000d7355
Aydoan Balakchiev <balakchiev02@gmail.com>
2023-03-12 02:00:10 +0200

    Add custom observable

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {Observable, Observer, of, Subscription} from "rxjs";
+
 import {AuthService} from "../auth.service";
 
 @Component({
@@ -7,12 +9,33 @@ import {AuthService} from "../auth.service";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private observable: Subscription;
 
   constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) {
   }
 
+  ngOnDestroy(): void {
+    this.observable.unsubscribe();
+  }
+
   ngOnInit() {
+    //observable:
+    const data = of(1);
+    const customIntervalObservable = new Observable((observer: Observer<any>) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
+    });
+
+    customIntervalObservable
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        alert(error)
+      });
   }
 
   onLoadServer(id: number) {
