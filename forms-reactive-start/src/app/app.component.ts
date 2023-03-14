ea0a8a07033330d8fee7ca4efea4c5925626c714
Dimitar Iliev <ilievd@aforbes.com>
2023-03-14 12:04:32 +0200

    Practice reactive forms

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
       'email': new FormControl(null),
       'gender': new FormControl(this.genders[0])
     })
