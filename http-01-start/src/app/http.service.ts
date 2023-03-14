5e57300ac2ce64ef929db5ff76e924f629737254
AydoanB <balakchiev02@gmail.com>
2023-03-14 18:31:38 +0200

    Add new code to service

--- http-01-start/src/app/http.service.ts
+++ http-01-start/src/app/http.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {IPost} from "./PostModel";
 import {Observable, Subject} from "rxjs";
 import {map} from "rxjs/operators";
@@ -12,22 +12,25 @@ const base = 'https://angular-the-complete-gui-80dd1-default-rtdb.europe-west1.f
 export class HttpService {
 
   error: Subject<any> = new Subject<any>();
+
   constructor(private http: HttpClient) {
   }
 
   addPost(post: IPost) {
     this.http.post(base,
-      post)
+      post, {
+        responseType: 'blob'
+      })
       .subscribe(respData => {
         console.log(respData)
-    }, error => this.error.next(error.message));
+      }, error => this.error.next(error.message));
   };
 
   getPosts(): Observable<any> {
-    return this.http.get(base).pipe(map((rawResponse) => Object.values(rawResponse)));
+    return this.http.get(base, {headers: new HttpHeaders({'Get': 'Too'})}).pipe(map((rawResponse) => Object.values(rawResponse)));
   }
 
-  deletePosts(): Observable<any>{
+  deletePosts(): Observable<any> {
     return this.http.delete(base);
   }
 }
