b99bb5019a05b508d2098a37bdec8142d98125f1
AydoanB <balakchiev02@gmail.com>
2023-03-14 13:11:26 +0200

    Delete tests file

--- pipes-start/src/app/pipes/word-shortener.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WordShortenerPipe } from './word-shortener.pipe';
-
-describe('WordShortenerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new WordShortenerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
