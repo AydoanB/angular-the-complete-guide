cf7533bfa40d8aef4006adc83cbac962024ec5a0
AydoanB <balakchiev02@gmail.com>
2023-03-20 18:32:24 +0200

    Modify user model

--- prj-http-01-save-and-fetch-recipes/src/app/shared/user.model.ts
+++ prj-http-01-save-and-fetch-recipes/src/app/shared/user.model.ts
@@ -1,4 +1,19 @@
-export interface User{
-  name: string,
-  password: string,
+export class User {
+  name: string;
+  password: string;
+
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpDate: Date
+  ) {
+  }
+
+  get token() {
+    if(!this._tokenExpDate || new Date() > this._tokenExpDate){
+      return null;
+    }
+    return this._token;
+  }
 }
