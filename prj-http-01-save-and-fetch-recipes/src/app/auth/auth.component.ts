83322f53359ac07d32f9082b5ff1291d2e5a12c1
AydoanB <balakchiev02@gmail.com>
2023-03-20 19:02:56 +0200

    Refactor and add spinner to auth component

--- prj-http-01-save-and-fetch-recipes/src/app/auth/auth.component.ts
+++ prj-http-01-save-and-fetch-recipes/src/app/auth/auth.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
+import {User} from "../shared/user.model";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -7,19 +11,43 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-  isLogged: boolean = false;
+  isInLoginMode: boolean = false;
+  isLoading: boolean = false;
+  error: string = undefined;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   ngOnInit() {
   }
 
   onSwitchMode() {
-    this.isLogged = !this.isLogged;
+    this.isInLoginMode = !this.isInLoginMode;
   }
 
   onSubmit(authForm: NgForm) {
-    console.log(authForm.value)
+    let user: User = authForm.value;
+
+    let obs: Observable<any>;
+
+    this.isLoading = true;
+    if (this.isInLoginMode) {
+      obs = this.authService.signIn(user);
+    } else {
+      obs = this.authService.signUp(user);
+    }
+
+    obs.subscribe((response) => {
+      console.log(response);
+      this.isLoading = false;
+
+      this.router.navigate(['/recipes']);
+    }, error => {
+      this.error = error;
+
+      this.isLoading = false;
+    });
+
+    authForm.reset();
   }
 }
