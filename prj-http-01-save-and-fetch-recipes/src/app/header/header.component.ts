291ed4e520cae4e77e10be7c4f4b9c04bf3980c1
AydoanB <balakchiev02@gmail.com>
2023-03-20 19:04:06 +0200

    Add dynamic authenticator validator to header component

--- prj-http-01-save-and-fetch-recipes/src/app/header/header.component.ts
+++ prj-http-01-save-and-fetch-recipes/src/app/header/header.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
-import { DataStorageService } from '../shared/data-storage.service';
+import {DataStorageService} from '../shared/data-storage.service';
+import {AuthService} from "../auth/auth.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated: boolean = false;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService, private router: Router) {
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -15,5 +22,21 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
+
+  onLogout() {
+    this.isAuthenticated = false;
+    this.router.navigate(['/sign-up']);
   }
 }
